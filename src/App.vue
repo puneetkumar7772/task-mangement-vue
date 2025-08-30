d15c22fd42f6e33ded2e3fdc111a75eee175be36
puneetkumar7772 <puneetkumar7772@gmail.com>
2025-08-30 22:14:03 +0530

    intial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <AppNavbar msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import AppNavbar from './components/AppNavbar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppNavbar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <h1>Vue js setup done</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppNavbar',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color:red;
+  font-size:30px
+}
+
+</style>
